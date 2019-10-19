8eb449bf714c7b12301dadbb12d54e98ca00da1a
Logon Aluno <logonaluno@local.com>
2019-10-19 16:28:05 -0300

    Add project files.

---
+++ Dashboard/Sales.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dashboard
+{
+    public class Sales
+    {
+        public Guid StoreId { get; set; }
+        public Decimal Total { get; set; }
+        public Decimal Value { get; set; }
+        
+    }
+}
---
+++ GeekBurger.Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekBurger.Dashboard.Controllers
+{
+    [Route("api/dashboard")]
+    public class DashboardController : ControllerBase
+    {
+        [HttpGet("{storeid}")]
+        public IActionResult
+        GetSalesByStoreId(Guid storeId)
+        {
+            return Ok();
+        }
+    }
+}
